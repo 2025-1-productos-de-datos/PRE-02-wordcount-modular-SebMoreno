31c98e1e042ac568c088ed63b136121a75d4c947
Sebastian Moreno <jumorenoru@unal.edu.co>
2025-05-03 12:04:32 -0500

    finish homework

---
+++ homework/tests/test_migracion.py
@@ -0,0 +1,26 @@
+import os
+
+import subprocess
+
+
+def test_migracion():
+    try:
+        subprocess.run(
+            ["python3", "-m", "homework", "data/input", "data/output"],
+            check=True,
+        )
+    except subprocess.CalledProcessError as e:
+        raise Exception(f"Error running the homework script: {e}")
+
+    if not os.path.exists("data/output/wordcount.tsv"):
+        raise FileNotFoundError("El archivo results.tsv no existe.")
+
+    results = {}
+    with open("data/output/results.tsv", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    for line in lines:
+        key, value = line.strip().split("\t")
+        results[key] = value
+
+    assert results.get("computational", 0) == "3"
+    assert results.get("analytics", 0) == "5"
